0a9f378d783de3b36b5c018e6d04b915527c2268
SHUKRANT PAWAR <shukrantpawar@SHUKRANTs-MacBook-Air.local>
2020-02-22 01:17:15 +0530

    Initial commit

---
+++ abcnsevenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/shukrantpawar/Desktop/Development/PythonProjects/NSEProject/abcnsevenv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
